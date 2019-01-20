21e00051f34b954bf35249a38bd24731ae67b5f2
Tobias Martens <tobiasmartens@Tobiass-MacBook-Air-2.local>
2019-01-20 19:35:44 +0100

    headline

--- js/main.js
+++ js/main.js
@@ -76,7 +76,7 @@ const app = function () {
 		posts.forEach(function (post) {
 			const article = document.createElement('article');
 			article.innerHTML = `
-				<hr class="articlebreak"><br><h2>${post.title}</h2>
+				<div class="yellow-box"></div></div><hr class="articlebreak"><br><h2>${post.title}</h2>
 				<div class="article-details">
 					<div>By ${post.author} on ${_formatDate(post.timestamp)}</div>
 					<div>Posted in ${post.category}</div>
