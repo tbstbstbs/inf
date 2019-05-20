59217fddac58aaa2a30fc405b09b4916020c6ac5
Tobias Martens <tm@whoelse.ai>
2019-05-20 08:31:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ const app = function () {
 			article.innerHTML = `
 				<div class="yellow-box"></div></div><hr class="articlebreak"><br><h2>${post.title}</h2>
 				<div class="article-details">
-					<div>By ${post.author} on ${_formatDate(post.timestamp)}</div>
+					<div>Source ${post.author} on ${_formatDate(post.timestamp)}</div>
 					<div>Posted in ${post.category}</div>
 				</div>
 				${_formatContent(post.content)}
