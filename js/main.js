be7c0ec3a9d3e43fae4dc65367904ad7463adffb
Tobias Martens <tm@whoelse.ai>
2019-05-21 12:08:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ const app = function () {
 	}
 
 	function _buildFilter () {
-	    page.filter.appendChild(_buildFilterLink('See All', true));
+	    page.filter.appendChild(_buildFilterLink('See All', false));
 
 	    CATEGORIES.forEach(function (category) {
 	    	page.filter.appendChild(_buildFilterLink(category, false));
